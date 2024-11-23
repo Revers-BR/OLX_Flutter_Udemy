089d45dbab4e20852af82a40fce55bcde836fefe
Revers-BR <reversrivair@gmail.com>
2024-11-23 08:38:37 -0300

    Adicionado filtros na tela inicial

--- lib/views/anuncios.dart
+++ lib/views/anuncios.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:olx_flutter/config/config.dart';
 import 'package:olx_flutter/models/itens_menu.dart';
 
 class Anuncios extends StatefulWidget {
@@ -15,6 +16,11 @@ class _Anuncios extends State<Anuncios> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   List<String> itensMenu = [];
+
+  String? _itemSelecionadoEstado;
+  String? _itemSelecionadoCategoria;
+  List<DropdownMenuItem<String>> _itensDropEstado = [];
+  List<DropdownMenuItem<String>> _itensDropCategoria = [];
   
   void _escolhaMenuItem(String itemEscolhido){
 
@@ -53,15 +59,23 @@ class _Anuncios extends State<Anuncios> {
     }
   }
 
+  void _carregaritensDropDown(){
+
+    _itensDropCategoria = Configuracoes.getCategorias();
+    _itensDropEstado    = Configuracoes.getEstados();
+  }
+
   @override
   void initState() {
     super.initState();
+    _carregaritensDropDown();
     _verificarUsuarioLogado();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         title: const Text("OLX"),
         elevation: 0,
@@ -78,6 +92,59 @@ class _Anuncios extends State<Anuncios> {
             },
           )
         ],
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                
+                Expanded(
+                  child:  DropdownButtonHideUnderline(
+                    child: Center(
+                      child: DropdownButton(
+                        items: _itensDropCategoria, 
+                        value: _itemSelecionadoCategoria,
+                        onChanged: (categoria) => setState(() {
+                          _itemSelecionadoCategoria = categoria;
+                        }),
+                        iconEnabledColor: const Color(0xff9c27b0),
+                        style: const TextStyle(
+                          fontSize: 22, color: Colors.black
+                        ),
+                      ),
+                    ),
+                  ) 
+                ),
+
+                Container(
+                  color: Colors.grey[200],
+                  width: 1, height: 60,
+                ),
+                
+                Expanded(
+                  child:  DropdownButtonHideUnderline(
+                    child: Center(
+                      child: DropdownButton(
+                        items: _itensDropEstado, 
+                        value: _itemSelecionadoEstado,
+                        onChanged: (estado) => setState(() {
+                          _itemSelecionadoEstado = estado;
+                        }),
+                        iconEnabledColor: const Color(0xff9c27b0),
+                        style: const TextStyle(
+                          fontSize: 22, color: Colors.black
+                        ),
+                      ),
+                    ),
+                  ) 
+                ),
+
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
